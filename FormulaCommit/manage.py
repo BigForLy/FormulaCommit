40570b7206ba131e56c66f84e36568705d5d12c2
BigForLy <oringan@mail.ru>
2022-02-08 18:07:51 +0500

    #2

--- FormulaCommit/manage.py
+++ FormulaCommit/manage.py
@@ -1,1 +1,37 @@
-def
+import graphlib
+import datetime
+
+from FormulaCommit.parse import ParseManager
+
+data = {"@d": "@a-@c",
+        "@ab": "@d+@c",
+        "@e": "round(@z, 0)",
+        "@z": "@a+@ab",
+        "@a": "5.4",
+        "@b": "10",
+        "@c": "@a+@b"}
+print(datetime.datetime.now())
+graph = {}
+
+parse_manager = ParseManager()
+
+for key, value in data.items():
+    graph.update({key: set(parse_manager.parses(value))})
+
+print(graph)
+
+ts = graphlib.TopologicalSorter(graph)
+z = tuple(ts.static_order())
+print(z)
+
+data2 = {}
+
+for param in z:
+    if param.find('@') == -1:  # число
+        data2.update({param: float(param)})
+    else:  # строка
+        data2.update({param: float(eval(parse_manager.calc(data[param], data2)))})
+
+
+print(data2)
+print(datetime.datetime.now())
--- FormulaCommit/parse.py
+++ FormulaCommit/parse.py
@@ -1,27 +1,52 @@
-OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
-             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+class ParseManager:
+
+    def __init__(self):
+        self.__OPERATORS = {'+', '-', '*', '/'}
+        self.__func = {'round'}
+        self.isString = False
+
+    def parses(self, formula_string):
+        self.isString = False
+        param = ''
+        for s in formula_string:
+            if s in '1234567890.' and not self.isString:  # number
+                param += s
+            elif s in self.__OPERATORS or s in "( ,)":  # garbage
+                if param and param in self.__func:
+                    param = ''
+                if param:
+                    yield param if self.isString else param  # float(param)
+                    param = ''
+                self.isString = False
+            else:  # string
+                self.isString = True
+                param += s
+        if param:
+            yield param if self.isString else param  # float(param)
+
+    def calc(self, formula_string, params):
+        self.isString = False
+        param = ''
+        calc_string = ''
+        for s in formula_string:
+            if s in '1234567890.' and not self.isString:  # number
+                param += s
+            elif s in self.__OPERATORS or s in "( ,)":  # garbage
+                if param:
+                    calc_string += str(params[param]) if params.get(param) and self.isString else param
+                    param = ''
+                calc_string += s
+                self.isString = False
+            else:  # string
+                self.isString = True
+                param += s
+        if param:
+            calc_string += str(params[param]) if params.get(param) and self.isString else param
+        return calc_string
 
 
-def parse(formula_string):
-    number = ''
-    param = ''
-    for s in formula_string:
-        if s in '1234567890.':  # если символ - цифра, то  число
-            number += s
-        elif s in OPERATORS or s in "()":
-            yield s
-        elif s in ' ':  # garbage
-            if number:
-                yield float(number)
-                number = ''
-        else:
-            param += s
 
 
-        # elif number:  # если символ не цифра, то выдаём собранное число и начинаем собирать заново
-        #     yield float(number)
-        #     number = ''
-        # if s in OPERATORS or s in "()":  # если символ - оператор или скобка, то выдаём как есть
-        #     yield s
-    if number:  # если в конце строки есть число, выдаём его
-        yield float(number)
+# a = ParseManager()
+# p = a.parses('@a + @b-@ab_2*@aab_1')
+# print(set(p))
